3e2b3fb03ca1d67a67a64070d6c2011554ddbd6b
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-23 19:55:07 -0300

    new render to task

---
+++ src/components/listTask/render.component.tsx
@@ -0,0 +1,62 @@
+import { TaskEntity } from "@/models/entities/task.entity"
+import ShowTask from "../showTask/showTask.component"
+import Page from "@/utils/pages/page"
+import ActionMenu from "../ActionMenu/render"
+import Btn from "../buttons/btn.component"
+import { FaTrash } from "react-icons/fa"
+import UseListTasks from "./service.listTask"
+import { FaExchangeAlt } from "react-icons/fa";
+
+
+export default function ListTasks() {
+    const { 
+        tasks, 
+        deleteTask,
+        changeStatus,
+        setQuery,
+        load
+    } = UseListTasks()
+
+    return (
+        <>
+            {load ? (
+                <div className="flex justify-center items-center min-h-screen">
+                    <div className="loader"></div>
+                </div>
+            ) : (
+                <>
+                    <div className="w-[85%] m-auto">
+                        {tasks.data.length > 0 ? (
+                            tasks.data.map((task: TaskEntity) => (
+                                <div key={task.id} className="flex flex-row items-center p-3 my-3 rounded-full border-1 border-white text-white">
+                                    <div className="basis-2/8 text-center">
+                                        <p>{task.name}</p>
+                                    </div>
+                                    <div className="basis-3/8 text-center">
+                                        <p>{task.description}</p>
+                                    </div>
+                                    <div className="basis-2/8">
+                                        <p>{task.IsComplete ? 'no completed' : 'completed'}</p>
+                                    </div>
+                                    <div className="basis-1/8 text-center">
+                                        <ActionMenu>
+                                            <Btn icon={<FaTrash />} funcClick={() => deleteTask(task.id)} margin={"my-2"} /> <br />
+                                            <Btn icon={<FaExchangeAlt />} funcClick={() => changeStatus(task.id)} margin={"my-2"} />
+                                        </ActionMenu>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div className="flex justify-center items-center min-h-screen">
+                                <h1>NO TASKS</h1>
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        {/* paginacao */}
+                    </div>
+                </>
+            )}
+        </>
+    );
+}
