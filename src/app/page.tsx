b2cc8595f5aaa1d10716c286f94bf655a65be47b
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-23 19:26:11 -0300

    render all tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+'use client'
+
+import Header from "@/components/header.component";
 import { Providers } from "./providers";
+import Btn from "@/components/buttons/btn.component";
+import ListTasks from "@/components/listTask/render.component";
+import UseHome from "./service";
+import { MdCreate } from "react-icons/md";
+
 
 export default function Home() {
+  
   return (
     <Providers>
+      <Header>
+        <Btn icon={ <MdCreate /> } title={""} funcClick={() => {alert('OK')}} />
+      </Header>
       <main>
-        <h1>HELLO WORLD</h1>
+        <ListTasks />
       </main>
     </Providers>
   );
