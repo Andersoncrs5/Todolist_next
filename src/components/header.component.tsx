485eca0c71d879f470dbf12b33bbb26d66c5d450
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-23 19:26:32 -0300

    Added new comoonent header

---
+++ src/components/header.component.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+interface Props {
+    children?: ReactNode;
+}
+
+export default function Header({ children }: Props) {
+    return (
+        <div className="flex flex-row items-center p-3 border-1 border-white">
+            <div className="basis-1/6 text-center">
+                <h2 className="text-2xl font-bold text-white">My Task</h2>
+            </div>
+            <div className="basis-4/6"></div>
+            <div className="basis-1/6 text-center">
+                {children}
+            </div>
+        </div>
+    );
+}
