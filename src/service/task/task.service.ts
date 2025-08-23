09ff0b917996fdc9a44eb6cc3f18a39b0e8f146f
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-23 19:57:18 -0300

    set Task service

---
+++ src/service/task/task.service.ts
@@ -0,0 +1,52 @@
+import { TaskEntity } from "@/models/entities/task.entity"
+import api from "../api"
+import { AxiosResponse } from "axios"
+import CreateTaskDTO from "@/models/dtos/CreateTaskDTO.dto"
+import ResponseBody from "@/utils/responses/ResponseBody.response"
+import Page from "@/utils/pages/page"
+import QueryParamsTask from "./queryParams"
+
+export default class TaskService {
+
+    private readonly url: string = "/v1/Todo"
+
+    public async getAll(query?: QueryParamsTask): Promise<Page<TaskEntity[]>> {
+        try {
+            const response = await api.get<Page<TaskEntity[]>>(this.url, {
+                params: query
+            });
+            
+            return response.data    
+        } catch(err) {
+            console.error(err)
+            throw err
+        }
+    }
+
+    async get(id: string) {
+
+    }
+
+    async create(dto: CreateTaskDTO): Promise<ResponseBody<TaskEntity>> {
+        try {
+            const response = await api.post<ResponseBody<TaskEntity>>(this.url, dto)
+
+            return response.data
+        } catch(err) {
+            console.log("error the create new task!\nError: " + err)
+            throw err
+        }
+    }
+
+    async delete(id: string): Promise<ResponseBody<TaskEntity>> {
+        try {
+            const response = await api.delete<ResponseBody<TaskEntity>>(this.url+"/"+id)
+
+            return response.data
+        } catch(err) {
+            console.log("error the create new task!\nError: " + err)
+            throw err
+        }
+    }
+
+}
