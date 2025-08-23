8b421186000291602a1b50eede4c90b428a43dda
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-23 19:54:18 -0300

    added service to home

---
+++ src/app/service.ts
@@ -0,0 +1,11 @@
+'use client'
+
+import { TaskEntity } from "@/models/entities/task.entity";
+import  TaskService  from "@/service/task/task.service"
+import Page from "@/utils/pages/page";
+import ResponseBody from "@/utils/responses/ResponseBody.response";
+import { useState, useEffect } from "react";
+
+export default function UseHome() {
+    
+}
