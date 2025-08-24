4bd67463fefa752463acf52c1646f302aa120a6f
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-24 18:31:04 -0300

    fix style

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -7,11 +7,11 @@ interface Props {
 export default function Header({ children }: Props) {
     return (
         <div className="flex flex-row items-center p-3 border-1 border-white">
-            <div className="basis-1/6 text-center">
+            <div className="basis-1/8 text-center">
                 <h2 className="text-2xl font-bold text-white">My Task</h2>
             </div>
-            <div className="basis-4/6"></div>
-            <div className="basis-1/6 text-center">
+            <div className="basis-6/8"></div>
+            <div className="basis-1/8 text-center">
                 {children}
             </div>
         </div>
