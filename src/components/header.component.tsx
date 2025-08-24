fdf4e9f038e8b1119d226f6b016d957f57654216
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-24 20:13:12 -0300

    more params to ReactNode

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,18 +1,21 @@
 import { ReactNode } from "react";
 
 interface Props {
-    children?: ReactNode;
+    middleContent?: ReactNode;
+    rightContent?: ReactNode;
 }
 
-export default function Header({ children }: Props) {
+export default function Header({ middleContent, rightContent }: Props) {
     return (
         <div className="flex flex-row items-center p-3 border-1 border-white">
             <div className="basis-1/8 text-center">
                 <h2 className="text-2xl font-bold text-white">My Task</h2>
             </div>
-            <div className="basis-6/8"></div>
+            <div className="basis-6/8 text-center py-2">
+                {middleContent}
+            </div>
             <div className="basis-1/8 text-center">
-                {children}
+                {rightContent}
             </div>
         </div>
     );
