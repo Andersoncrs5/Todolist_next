bd5cb20d350b621e94f05964c82e2868b8712b9d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-24 18:37:01 -0300

    improvement style

--- src/components/showAlert/render.tsx
+++ src/components/showAlert/render.tsx
@@ -1,36 +1,32 @@
-type BgStyle = 
-  | 'bg-red-500'
-  | 'bg-white'
-  | 'bg-yellow-500'
-  | 'bg-green-500'
-  | 'bg-transparent';
-
-type TextStyle = 
-  | 'text-red-500'
-  | 'text-white'
-  | 'text-yellow-500'
-  | 'text-green-500'
-  | 'text-black'; 
+import { BgStyle, BorderStyle, TextStyle } from "./types";
 
 interface Props {
     msg: string;
     bgStyle: BgStyle;    
     textStyle: TextStyle;  
+    border: BorderStyle
 }
 
 export default function ShowAlert(props: Props) {
     return (
         <div 
-            className="
+            className={`
                 absolute top-[2%]         
-                left-1/2  -translate-x-1/2 
+                left-1/2 -translate-x-1/2 
                 z-50 w-[80%] 
-                text-center border-2 
-                border-white mx-auto 
-                mt-2 rounded-lg p-2
-            " 
+                text-center
+                border ${props.border}
+                mt-2 rounded p-2
+                ${props.bgStyle}
+            `} 
         >
-            <p className={`my-1 ${props.bgStyle} ${props.textStyle}`} >{props.msg}</p>
+            <p className={`my-1 ${props.textStyle}`} >{props.msg}</p>
         </div>
     )
 }
+
+
+
+
+
+
