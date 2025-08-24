69e99c9fd3588999d884242c06c0fe93504b009d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-24 14:38:41 -0300

    fix anything

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,11 +1,3 @@
 'use client'
 
-import { TaskEntity } from "@/models/entities/task.entity";
-import  TaskService  from "@/service/task/task.service"
-import Page from "@/utils/pages/page";
-import ResponseBody from "@/utils/responses/ResponseBody.response";
-import { useState, useEffect } from "react";
-
-export default function UseHome() {
-    
-}
+export default function UseHome() {}
