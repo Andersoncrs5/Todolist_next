5e4b36ea712703cc26f54e175d1594faa810e13d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-23 21:59:02 -0300

    more style

--- src/components/buttons/btn.component.tsx
+++ src/components/buttons/btn.component.tsx
@@ -6,14 +6,17 @@ interface Types {
     title?: string
     funcClick: () => any,
     icon?: any,
-    margin?: string
+    margin?: string,
+    style?: string,
+    disabled?: boolean
 }
 
-export default function Btn({ title, funcClick, icon, margin }: Types) {
+export default function Btn({ title, funcClick, icon, margin, disabled,style }: Types) {
     return (
         <button
-            className={`bg-transparent border-1 border-white p-2 rounded-full text-white hover:bg-white hover:text-black ${margin}`}
+            className={`bg-transparent border-1 border-white p-2 rounded-full text-white hover:bg-white hover:text-black ${margin} ${style}`}
             onClick={funcClick}
+            disabled={disabled}
         >
             {icon} {title}
         </button>
