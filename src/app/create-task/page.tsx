081bd7594e980515334284e48ba7c5ee62caf9e6
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-24 17:30:37 -0300

    Create tasks system

---
+++ src/app/create-task/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+import CustomInput from "@/components/input/render";
+import { Providers } from "../providers";
+import UseCreateTask from "./service";
+import CustomTextarea from "@/components/textarea/render";
+import BtnSubmit from "@/components/BtnSubmit/render";
+import BtnUrl from "@/components/btnUrl/render";
+import ShowAlert from "@/components/showAlert/render";
+import ShowAlerts from "@/components/showAlerts/render";
+
+export default function CreateTask() {
+    const {
+        name,
+        setName,
+        description,
+        setDescription,
+        isSubmitting,
+        handleSubmit,
+        colorAlert,
+        colorBorderAlert,
+        colorTextAlert,
+        msg,
+        alert,
+        errorForm,
+        msgErrorForm,
+    } = UseCreateTask()
+
+    return (
+        <Providers>
+            <div className="flex justify-center items-center min-h-screen text-white p-4">
+                {alert && <ShowAlert msg={msg} bgStyle={colorAlert} textStyle={colorTextAlert} border={colorBorderAlert} /> }
+                {errorForm && <ShowAlerts msgs={msgErrorForm} bgStyle={"bg-transparent"} textStyle={"text-red-500"} border={"border-red-500"} /> }
+
+                <form onSubmit={handleSubmit} className="w-full max-w-lg p-8 rounded-lg shadow-2xl border border-white">
+                    
+                    <h3 className="text-3xl font-bold mb-3 text-center">
+                       CREATE TASK
+                    </h3>
+
+                    <div className="flex flex-col gap-6">
+                        <div className="flex flex-col">
+                            <CustomInput 
+                                // min={5}
+                                // max={50}
+                                value={name}
+                                nameLabel={"Título"}
+                                onChange={(e) => setName(e.target.value)}
+                                type={"text"}
+                                placeholder="Escreva o título da tarefa"
+                                border="border-white border" 
+                                required={true}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-6">
+                            <CustomTextarea 
+                                value={description} 
+                                nameLabel={"Description"}
+                                onChange={(e) => setDescription(e.target.value) }
+                                placeholder="Write the description"
+                                more={"border-white border"}
+                                rows={3}
+                            />
+                        </div>
+                        
+                        <div className="flex justify-end">
+                            <BtnUrl url={"/"}  bgColor={"bg-transparent"} name={"BACK"} style={"mx-2 hover:bg-white border-white hover:text-black p-1"} />
+                            <BtnSubmit bgColor={"bg-transparent"} more="hover:bg-white hover:text-black" isSubmitting={isSubmitting} />
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </Providers>
+    );
+}
---
+++ src/app/create-task/service.ts
@@ -0,0 +1,131 @@
+'use client'
+import { BgStyle, BorderStyle, TextStyle } from "@/components/showAlert/types";
+import CreateTaskDTO from "@/models/dtos/CreateTaskDTO.dto";
+import { TaskEntity } from "@/models/entities/task.entity";
+import api from "@/service/api";
+import TaskService from "@/service/task/task.service";
+import ResponseBody from "@/utils/responses/ResponseBody.response";
+import ResponseError from "@/utils/responses/ResponseError";
+import { AxiosError, AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function UseCreateTask() {
+    const timeMsg = 6000
+    const router = useRouter()
+
+    const [name, setName] = useState<string>('')
+    const [description, setDescription] = useState<string>('')
+
+    const [msg, setMsg] = useState<string>('');
+
+    const [colorAlert, setColorAlert] = useState<BgStyle>("bg-transparent");
+    const [colorTextAlert, setColorTextAlert] = useState<TextStyle>("text-white");
+    const [colorBorderAlert, setColorBorderAlert] = useState<BorderStyle>("border-white");
+
+    const [alert, setAlert] = useState<boolean>(false);
+
+    const [errorForm, setErrorForm] = useState<boolean>(false);
+
+    const [msgErrorForm, setMsgErrorForm] = useState<string[]>([]);
+
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault()
+        setIsSubmitting(true)
+
+        const dto: CreateTaskDTO = { name, description } as CreateTaskDTO
+
+        try {
+            const response: AxiosResponse<ResponseBody<TaskEntity>, any> = await api.post('/v1/Todo', dto)
+
+            if (
+                response.data.code === 200 || response.data.code >= 202 && response.data.code <= 299
+            ) {
+                showAlertFunc(response.data.message, "bg-transparent", "text-green-500", "border-green-500")
+            }
+
+
+            if (response.data.code === 201) {
+                showAlertFunc(response.data.message, "bg-transparent", "text-green-500", "border-green-500")
+
+                setTimeout(() => {
+                    router.push('/')
+                }, timeMsg)
+            }
+
+        } catch(error) {
+            const err = error as AxiosError<ResponseError>;
+            console.log(err)
+            const status = err.response?.data.status
+
+            if (!status) {
+                showAlertFunc("Error the create task! Please try again later", "bg-transparent", "text-red-500", "border-red-500")
+                router.push("/")
+                return 
+            }
+
+            if (status === 400) {
+                showErrorMsgForm(err.response?.data.errors.Name as string[] , "bg-transparent", 'text-red-500', 'border-red-500')
+            }
+
+            if (status >= 500 &&  status <= 599 ) {
+                showAlertFunc("Error The to create task! Please try again later" , "bg-transparent", 'text-red-500', 'border-red-500')
+                router.push("/")
+            }
+
+        } finally {
+            clearInputs()
+            setIsSubmitting(false)
+        }
+    }
+
+    function showAlertFunc(msg: string, bg: BgStyle, text: TextStyle, border: BorderStyle) {
+        setAlert(true)
+        setColorAlert(bg)
+        setColorTextAlert(text)
+        setColorBorderAlert(border)
+
+        setMsg(msg)
+
+        setTimeout(() =>{
+            setAlert(false)
+        }, timeMsg)
+    }
+
+    function showErrorMsgForm(msg: string[], bg: BgStyle, text: TextStyle, border: BorderStyle) {
+        
+        setErrorForm(true)
+        setColorAlert(bg)
+        setColorTextAlert(text)
+        setColorBorderAlert(border)
+
+        setMsgErrorForm(msg)
+
+        setTimeout(() =>{
+            setErrorForm(false)
+        }, timeMsg)
+    }
+
+    function clearInputs() {
+        setDescription('')
+        setName('')
+    }
+
+    return {
+        name,
+        description,
+        setName,
+        setDescription,
+        isSubmitting,
+        colorAlert,
+        colorTextAlert,
+        colorBorderAlert,
+        handleSubmit,
+        msg,
+        alert,
+        errorForm,
+        msgErrorForm
+    }
+}
