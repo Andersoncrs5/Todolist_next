9974bdfc580a0cb2d0e13461ac5532e320c0d447
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-23 21:59:39 -0300

    added pagination and handler err

--- src/components/listTask/service.listTask.ts
+++ src/components/listTask/service.listTask.ts
@@ -11,12 +11,19 @@ export default function UseListTasks() {
     const [tasks, setTasks] = useState<Page<TaskEntity[]>>(
         {pageNumber : 0,pageSize: 0,totalRecords: 0,code: 0,totalPages: 0,data: []} as Page<TaskEntity[]> );
     
-    const [query, setQuery] = useState<QueryParamsTask>();
+    const [query, setQuery] = useState<QueryParamsTask>({
+        PageNumber: 1,
+        PageSize: 10
+    } as QueryParamsTask);
+            
     const [load, setLoad] = useState<boolean>(true);
+
+    const [alert, setAlert] = useState<boolean>(false);
+    const [msg, setMsg] = useState<string>('');
 
     useEffect(() => {
         fetchTasks();
-    }, []);
+    }, [query]);
 
     const fetchTasks = async () => {
         setLoad(true)
@@ -31,11 +38,50 @@ export default function UseListTasks() {
         const response: ResponseBody<TaskEntity> = await tasksService.delete(id)
         
         fetchTasks()
+        
+        if (response.code >= 200 || response.code <= 299) {
+            showErr(response.message)
+        }
+
+        if (response.code >= 400 || response.code <= 499) {
+            showErr(response.message)
+        }
+
+
+    }
+
+    async function changeStatus(id: string) {
+        const response: ResponseBody<TaskEntity> = await tasksService.changeStatus(id)
+
         return response
     }
 
-    async function changeStatus(id: String) {
+    function nextPage() {
+        if (tasks.pageNumber < tasks.totalPages) {
+            setQuery(prevQuery => ({
+                ...prevQuery,
+                PageNumber: (prevQuery.PageNumber || 1) + 1,
+            }));
+        }
+    }
 
+    function backPage() {
+        if (tasks.pageNumber > 1) {
+            setQuery(prevQuery => ({
+                ...prevQuery,
+                PageNumber: Math.max(1, (prevQuery.PageNumber || 1) - 1),
+            }));
+        }
+    }
+
+    function showErr(msg: string) {
+        setAlert(true)
+
+        setMsg(msg)
+
+        setTimeout(() =>{
+            setAlert(false)
+        }, 5000)
     }
 
     return {
@@ -44,6 +90,10 @@ export default function UseListTasks() {
         changeStatus,
         setQuery,
         query,
-        load
+        load,
+        backPage,
+        nextPage,
+        alert,
+        msg
     };
 }
