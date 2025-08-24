f39c0f28ccd6da7b51308fa3305ceb76d4f1b227
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-24 20:15:34 -0300

    Set types to components BgStyle, TextStyle and BorderStyle

---
+++ src/components/showAlert/types.ts
@@ -0,0 +1,20 @@
+export type BgStyle = 
+  | 'bg-red-500'
+  | 'bg-white'
+  | 'bg-yellow-500'
+  | 'bg-green-500'
+  | 'bg-transparent';
+
+export type TextStyle = 
+  | 'text-red-500'
+  | 'text-white'
+  | 'text-yellow-500'
+  | 'text-green-500'
+  | 'text-black'; 
+
+export type BorderStyle =
+  | 'border-red-500'
+  | 'border-white'
+  | 'border-yellow-500'
+  | 'border-green-500'
+  | 'border-black'; 
